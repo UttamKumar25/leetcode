42b0f8a2632768486d4f4eb426f91b4ff5b7929a
UttamKumar25 <126788770+UttamKumar25@users.noreply.github.com>
2024-12-07 17:32:36 +0530

    Time: 42 ms (77.79%), Space: 81.8 MB (43.89%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
+        });
+        
+        // Apply Longest Increasing Subsequence (LIS) on heights
+        vector<int> dp;
+        for (const auto& envelope : envelopes) {
+            int height = envelope[1];
+            auto it = lower_bound(dp.begin(), dp.end(), height);
+            if (it == dp.end()) {
+                dp.push_back(height);
+            } else {
+                *it = height;
+            }
+        }
+        return dp.size();
+    }
+};
