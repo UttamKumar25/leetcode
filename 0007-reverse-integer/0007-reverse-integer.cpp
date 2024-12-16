a946b358c095287d97a390f1e0c92ef6e433adf4
UttamKumar25 <126788770+UttamKumar25@users.noreply.github.com>
2024-12-16 09:24:49 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.23%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
+
+        string s = to_string(x);
+        if(x<0)
+            std::reverse(s.begin()+1, s.end());
+        else
+            std::reverse(s.begin(), s.end());
+        cout<<s<<" ";
+        return (stol(s)>INT_MAX || stol(s)<INT_MIN) ?0:stoi(s);
+        /*
         std::string s = std::to_string(x); // Explicit std:: namespace for clarity
         bool isNegative = (s[0] == '-');
         
@@ -21,7 +30,7 @@ public:
             return reversedNum;
         } catch (std::out_of_range&) {
             return 0; // Return 0 in case of overflow
-        }
+        }  */
     }
     
 };
