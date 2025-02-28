4f18edf015b721dde458bd011a06901ad45a67cb
UttamKumar25 <126788770+UttamKumar25@users.noreply.github.com>
2025-02-28 10:24:55 +0530

    Time: 2 ms (27.06%), Space: 11.1 MB (65.72%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int,int>mp;
+        for(int num: arr1){  //arr1 ko map me daaalo
+            mp[num]++;
+        }
+        vector<int>v;
+        //travserdse arr2
+        for(int num: arr2){
+            while(mp[num]>0){  //jab tak element hai tab tak nikalo
+                v.push_back(num);
+                mp[num]--;
+            }
+        }
+
+        //remning ko nikalo
+        vector<int>rem;
+        for(auto it: mp){
+            while(it.second>0){
+                rem.push_back(it.first);
+                it.second--;
+            }
+        }
+
+        sort(rem.begin(), rem.end());
+
+        v.insert(v.end(), rem.begin(), rem.end());
+        return v;
+
+
+    /*
       // Step 1: Populate the HashMap with Frequencies
     unordered_map<int, int> freqMap;
     for (int num : arr1) {
@@ -32,5 +61,8 @@ public:
 
     // Step 5: Return the Result Array
     return result;   
+    } 
+    */
+
     }
 };
