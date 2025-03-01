4e2dd4a76fe2a096f0b56059b6663b8ded7c16ec
UttamKumar25 <126788770+UttamKumar25@users.noreply.github.com>
2025-03-01 10:39:41 +0530

    Time: 3 ms (28.15%), Space: 9.4 MB (5.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,32 +5,11 @@ public:
         string s = to_string(x);
         if(x<0)
             std::reverse(s.begin()+1, s.end());
-        else
+        else //if +ve
             std::reverse(s.begin(), s.end());
-        cout<<s<<" ";
+        
         return (stol(s)>INT_MAX || stol(s)<INT_MIN) ?0:stoi(s);
-        /*
-        std::string s = std::to_string(x); // Explicit std:: namespace for clarity
-        bool isNegative = (s[0] == '-');
         
-        std::reverse(s.begin(), s.end()); // Explicitly use std::reverse
-        
-        // Build the reversed string
-        std::string ans = "";
-        if (isNegative) {
-            ans += '-';
-            ans += s.substr(0, s.length() - 1); // Skip the '-' in the reversed string
-        } else {
-            ans = s;
-        }
-        
-        // Convert to integer and check for overflow
-        try {
-            int reversedNum = std::stoi(ans); // Explicit std:: namespace for stoi
-            return reversedNum;
-        } catch (std::out_of_range&) {
-            return 0; // Return 0 in case of overflow
-        }  */
     }
     
 };
