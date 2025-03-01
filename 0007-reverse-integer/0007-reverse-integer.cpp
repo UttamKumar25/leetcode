e11c9d166ecdc81e10849a702f7b29b40b30674c
UttamKumar25 <126788770+UttamKumar25@users.noreply.github.com>
2025-03-01 10:46:04 +0530

    Time: 6 ms (5.67%), Space: 8.4 MB (82.33%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int reverse(int x) {
+        long sum = 0;
 
-        string s = to_string(x);
+    while (x != 0) {  // THIS METHOD HANDLE FOR BOTH +VE and -ve AS WELL FOR REVERSE
+        int rem = x % 10;
+        sum = sum * 10 + rem;
+        x = x / 10;
+    }
+    if (sum > INT_MAX || sum < INT_MIN) {// if REV goes beyond 2^31 return 0 {GIVEN IN QUES}
+        return 0;
+    }
+
+    return (int)sum;
+        /*string s = to_string(x);
         if(x<0)
             std::reverse(s.begin()+1, s.end());
         else //if +ve
             std::reverse(s.begin(), s.end());
         
-        return (stol(s)>INT_MAX || stol(s)<INT_MIN) ?0:stoi(s);
+        return (stol(s)>INT_MAX || stol(s)<INT_MIN) ?0:stoi(s);*/
         
     }
     
